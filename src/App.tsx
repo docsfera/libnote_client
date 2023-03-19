e2c46ecf09e7dfd836dcdbdbbd0865963a90d552
docsfera <tankionlineGit>
2023-03-20 04:07:26 +0500

    babel-polyfill

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, {useEffect, useRef} from 'react'
+import 'babel-polyfill'
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Aside from "./components/Aside/Aside"
