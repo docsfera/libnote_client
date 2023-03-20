b3d099f6350cc731031a938c5a99b17e168f7d42
docsfera <tankionlineGit>
2023-03-20 05:02:42 +0500

    simple application

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useRef} from 'react'
-import "@babel/polyfill";
+//import "@babel/polyfill";
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Aside from "./components/Aside/Aside"
@@ -19,25 +19,27 @@ function App() {
     return (
         <AuthProvider>
             <AuthContext.Consumer>
-                {value => value.userInfo.token
-                    ?
-                    <div className="App">
-                        <Aside/>
-                        <Routes>
-                            <Route path='/' element={<Main/>}/>
-                            <Route path='/books' element={<Books/>}/>
-                            <Route path='/notes' element={<Notes/>}/>
-                            <Route path='/folders' element={<Folders/>}/>
-                            <Route path='/pdf-viewer/:userId' element={<PdfViewer/>}/>
-                            <Route path='/note-creator' element={<NoteCreator/>}/>
-                            <Route path='/note-creator/:id' element={<NoteCreator/>}/>
-                            <Route path='/folder-notes/:id' element={<FolderNotes/>}/>
-                            <Route path='/book-settings/:id' element={<BookSettings/>}/>
-                            <Route path='/settings' element={<UserSettings/>}/>
-                        </Routes>
-                    </div>
-                    : <AuthWrapper/>
-                }
+                {value => <AuthWrapper/>}
+
+                {/*{value => value.userInfo.token*/}
+                {/*    ?*/}
+                {/*    <div className="App">*/}
+                {/*        <Aside/>*/}
+                {/*        <Routes>*/}
+                {/*            <Route path='/' element={<Main/>}/>*/}
+                {/*            <Route path='/books' element={<Books/>}/>*/}
+                {/*            <Route path='/notes' element={<Notes/>}/>*/}
+                {/*            <Route path='/folders' element={<Folders/>}/>*/}
+                {/*            <Route path='/pdf-viewer/:userId' element={<PdfViewer/>}/>*/}
+                {/*            <Route path='/note-creator' element={<NoteCreator/>}/>*/}
+                {/*            <Route path='/note-creator/:id' element={<NoteCreator/>}/>*/}
+                {/*            <Route path='/folder-notes/:id' element={<FolderNotes/>}/>*/}
+                {/*            <Route path='/book-settings/:id' element={<BookSettings/>}/>*/}
+                {/*            <Route path='/settings' element={<UserSettings/>}/>*/}
+                {/*        </Routes>*/}
+                {/*    </div>*/}
+                {/*    : <AuthWrapper/>*/}
+                {/*}*/}
             </AuthContext.Consumer>
         </AuthProvider>
     )
